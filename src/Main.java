670c4ee1526834869e73249fec6832664a6083ef
Antonioni17 <antonioni17@yandex.ru>
2023-02-19 14:17:32 +0300

    correct1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int ticketprice = 2000;
-        int bonusprice = 256;
+        int bonusprice = 20;
         int bonus =  ticketprice / bonusprice;
         System.out.print ("Бонусных миль : " + bonus);
     }
