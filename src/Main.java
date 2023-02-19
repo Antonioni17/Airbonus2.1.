c6d7af4ec3e71425a6bde1ac698a1aa901c17055
Antonioni17 <antonioni17@yandex.ru>
2023-02-19 14:13:11 +0300

    gitignore

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketprice = 2000;
+        int bonusprice = 256;
+        int bonus =  ticketprice / bonusprice;
+        System.out.print ("Бонусных миль : " + bonus);
+    }
+}
